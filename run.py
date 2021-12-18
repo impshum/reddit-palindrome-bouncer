86893dfb1c0d45674b3e42baa19531d4404585f4
impshum <impshum@impshum.co.uk>
2021-12-18 19:18:36 +0000

    Merger

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import configparser
 import re
 import sqlite3
 from sqlite3 import Error
+from unidecode import unidecode
 
 
 config = configparser.ConfigParser()
@@ -12,13 +13,17 @@ reddit_pass = config['REDDIT']['reddit_pass']
 reddit_client_id = config['REDDIT']['reddit_client_id']
 reddit_client_secret = config['REDDIT']['reddit_client_secret']
 reddit_target_subreddit = config['REDDIT']['reddit_target_subreddit']
+reddit_post_limit = int(config['REDDIT']['reddit_post_limit'])
+reddit_reply_text = config['REDDIT']['reddit_reply_text']
+test_mode = config['SETTINGS'].getboolean('test_mode')
+read_database = config['SETTINGS'].getboolean('read_database')
 
 reddit = praw.Reddit(
     username=reddit_user,
     password=reddit_pass,
     client_id=reddit_client_id,
     client_secret=reddit_client_secret,
-    user_agent='Something (by u/impshum)'
+    user_agent='Reddit Palindrome Bouncer (by u/impshum)'
 )
 
 
@@ -30,6 +35,8 @@ def db_connect():
                         post_id TEXT NOT NULL,
                         post_author TEXT NOT NULL,
                         post_date TIMESTAMP NOT NULL,
+                        post_text TEXT NOT NULL,
+                        post_length INTEGER NOT NULL,
                         palindrome INTEGER NOT NULL
                         );"""
         conn.execute(create_table)
@@ -39,12 +46,12 @@ def db_connect():
     return None
 
 
-def insert_row(conn, post_id, post_date, post_author, palindrome):
+def insert_row(conn, post_id, post_author, post_date, post_text, post_length, palindrome):
     cur = conn.cursor()
-    cur.execute("SELECT 1 FROM posts WHERE post_id = ? LIMIT 1", (post_id,))
+    cur.execute("SELECT 1 FROM posts WHERE post_id = ? LIMIT 1;", (post_id,))
     if not cur.fetchone():
         conn.execute(
-            "INSERT INTO posts (post_id, post_date, post_author, palindrome) VALUES (?, ?, ?, ?);", (post_id, post_date, post_author, palindrome))
+            "INSERT INTO posts (post_id, post_author, post_date, post_text, post_length, palindrome) VALUES (?, ?, ?, ?, ?, ?);", (post_id, post_author, post_date, post_text, post_length, palindrome))
         return True
 
 
@@ -57,7 +64,7 @@ def read_db(conn):
 
 
 def stripper(s):
-    return re.sub('[^A-Za-z0-9]+', '', s.lower().strip())
+    return re.sub('[^A-Za-z0-9]+', '', unidecode(s.lower().strip()))
 
 
 def is_palindrome(s):
@@ -67,25 +74,41 @@ def is_palindrome(s):
 def main():
     conn = db_connect()
 
-    for submission in reddit.subreddit(reddit_target_subreddit).new(limit=None):
-        if submission.author != None:
-            palindrome = is_palindrome(stripper(submission.title))
-            print(f'{submission.title} - {palindrome}')
-            if insert_row(conn, submission.id, submission.created, submission.author.name, palindrome):
-                if not palindrome:
-                    submission.mod.remove()
+    if read_database:
+        read_db(conn)
+        return
+
+    if test_mode:
+        print('TEST MODE')
+
+    for submission in reddit.subreddit(reddit_target_subreddit).new(limit=reddit_post_limit):
+        if submission.author is not None:
+            if submission.author.name not in ['AutoModerator', reddit_user]:
+                palindrome = is_palindrome(stripper(submission.title))
+                print(f'{palindrome} - {submission.title.strip()}')
+                if not test_mode:
+                    if insert_row(conn, submission.id, submission.author.name, submission.created, submission.title.strip(), len(submission.title), palindrome):
+                        if palindrome:
+                            submission.mod.approve()
+                        else:
+                            submission.reply(reddit_reply_text)
+                            submission.mod.remove()
 
         for comment in submission.comments.list():
-            if comment.author != None:
-                palindrome = is_palindrome(stripper(comment.body))
-                print(f'{comment.body} - {palindrome}')
-                if insert_row(conn, comment.id, comment.created, comment.author.name, palindrome):
-                    if not palindrome:
-                        comment.mod.remove()
+            if comment.author is not None:
+                if submission.author.name not in ['AutoModerator', reddit_user]:
+                    palindrome = is_palindrome(stripper(comment.body))
+                    print(f'{palindrome} - {comment.body.strip()}')
+                    if not test_mode:
+                        if insert_row(conn, comment.id, comment.author.name, comment.created, comment.body.strip(), len(comment.body), palindrome):
+                            if palindrome:
+                                comment.mod.approve()
+                            else:
+                                comment.reply(reddit_reply_text)
+                                comment.mod.remove()
 
     conn.commit()
 
 
 if __name__ == '__main__':
-    # read_db(conn)
     main()
